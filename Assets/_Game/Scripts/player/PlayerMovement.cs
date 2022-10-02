fd8fa48be1373f07e287c8c5a8bdff4c0f524a86
Nick Maclean <nicolasimaclean@gmail.com>
2022-10-02 01:18:25 -0500

    updated organization and consolidated code style

--- Assets/_Game/Scripts/player/PlayerMovement.cs
+++ Assets/_Game/Scripts/player/PlayerMovement.cs
@@ -1,90 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
-namespace Game.Player {
+namespace Game.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        [Min(0)]
+        float _movementSpeed = 5f;
 
-    public class PlayerMovement : MonoBehaviour {
-        // Enter public variables here
-        public float speed = 5f; // variable number that dictates the speed in which the player character moves
-        public PlayerInputActions playerControls; // Input control system for player movement controls
-
-
-
-        // Enter private variables here
-        private InputAction move;
-        private Vector3 movementVector = Vector3.zero;
-        private Vector2 aim;
-        private Rigidbody body;
+        PlayerInputActions playerControls;
+        InputAction move;
         
-        
+        Vector3 movementVector = Vector3.zero;
+        Vector2 aim;
+        Rigidbody body;
 
         /// <summary>
         /// Initialize the playerControls object on startup
         /// </summary>
-        private void Awake() {
+        void Awake()
+        {
             playerControls = new PlayerInputActions();
-            
         }
 
         /// <summary>
         /// Required method for new Unity input system. Activates the playerControls input action system.
         /// </summary>
-        private void OnEnable() {
+        void OnEnable()
+        {
             move = playerControls.Player.Move;
             move.Enable();
-         
         }
 
         /// <summary>
         /// Required method for new Unity input system. Disables the playerControls input action system
         /// </summary>
-        private void OnDisable() {
+        void OnDisable()
+        {
             move.Disable();
-            
         }
 
-        // Start is called before the first frame update
-        void Start() {
+        void Start()
+        {
             body = GetComponent<Rigidbody>();
-          
         }
 
-        // Update is called once per frame
-        void Update() {
+        void Update()
+        {
             // Execute player movement tasks
             GetMousePosition();
             GetPlayerInput();
             LookAt(aim);
-
-
         }
 
-        // FixedUpdate is called once per fixed real-time frame update. Used for physics processes.
-        private void FixedUpdate() {
-
+        void FixedUpdate()
+        {
             MovePlayer();
-            
         }
 
         /// <summary>
         /// Get current position of the mouse (if using mouse & keyboard) and calculate the angle the player should rotate to. Works well with WASD keys.
         /// </summary>
-        private void GetMousePosition() {
+        void GetMousePosition()
+        {
             Ray ray = Camera.main.ScreenPointToRay(aim);
             Plane s_plane = new (Vector3.up, Vector3.zero);
-            float rayDistance;
             
-            if (s_plane.Raycast(ray, out rayDistance))
+            if (s_plane.Raycast(ray, out float rayDistance))
             {
                 Vector3 point = ray.GetPoint(rayDistance);
                 LookAt(point);
             }
         }
 
-        private void LookAt(Vector3 lookPoint)
+        void LookAt(Vector3 lookPoint)
         {
             Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
             transform.LookAt(heightCorrectedPoint);
@@ -93,15 +85,17 @@ namespace Game.Player {
         /// <summary>
         /// Get keyboard from the player from keyboard or controller. Override mouse angle if using keyboard only or controller
         /// </summary>
-        private void GetPlayerInput() {
+        void GetPlayerInput()
+        {
             movementVector = move.ReadValue<Vector3>(); // read the player input for movement direction
         }
 
         /// <summary>
         /// Use the calculated movementVector to move the player accordingly
         /// </summary>
-        private void MovePlayer() {
-            body.velocity = new Vector3(movementVector.x * speed, 0f, movementVector.z * speed);
+        void MovePlayer()
+        {
+            body.velocity = new Vector3(movementVector.x * _movementSpeed, 0f, movementVector.z * _movementSpeed);
         }
     }
 }
