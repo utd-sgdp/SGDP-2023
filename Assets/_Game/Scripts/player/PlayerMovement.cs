a9f2d3bccf8882174ce877ba7ced54d1110fe452
Nick Maclean <nicolasimaclean@gmail.com>
2022-10-04 19:29:41 -0500

    organization and refactor of PlayerMovement

--- Assets/_Game/Scripts/player/PlayerMovement.cs
+++ Assets/_Game/Scripts/player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,112 +6,119 @@ using UnityEngine.InputSystem;
 
 namespace Game.Player
 {
-    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(PlayerInput), typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField]
         [Min(0)]
-        float _movementSpeed = 5f;
-        Vector3 _movementVector = Vector3.zero;
-        Vector2 _aim;
+        float _movementSpeed = 500f;
+        
+        [Header("Look")]
+        [SerializeField]
+        [Range(0, 1)]
+        float  _lookDeadZone = .1f;
+        
         Rigidbody _body;
         PlayerInput _input;
+        InputAction _moveAction;
+        InputAction _lookAction;
+        
+        static Plane s_groundPlane = new (Vector3.up, Vector3.zero);
 
-        /// <summary>
-        /// Initialize the PlayerInput object on startup
-        /// </summary>
+        #region MonoBehaviour
         void Awake()
         {
-            _input = GetComponent<PlayerInput>();
-        }
-
-        /// <summary>
-        /// Method called when object becomes active and enabled
-        /// </summary>
-        void OnEnable()
-        {
-            SubscribeInputs();
-        }
-
-        /// <summary>
-        /// Sets up and enables the PlayerInput actions. Abstracts the process to OnEnable
-        /// </summary>
-        void SubscribeInputs() 
-        {
-            _input.actions["Move"].performed += OnMove;
-        }
-
-        /// <summary>
-        /// Method called when object is disabled for destroyed
-        /// </summary>
-        void OnDisable()
-        {
-            UnsubscribeInputs();
-        }
-
-        /// <summary>
-        /// Disables PlayerInput actions. Abstracts the process to OnDisable
-        /// </summary>
-        void UnsubscribeInputs()
-        {
-            _input.actions["Move"].performed -= OnMove;
-        }
-
-        void OnMove(InputAction.CallbackContext context) 
-        {
-            _movementVector = context.ReadValue<Vector3>();
-        }
-
-        void Start()
-        {
             _body = GetComponent<Rigidbody>();
+            
+            _input = GetComponent<PlayerInput>();
+            _moveAction = _input.actions["Move"];
+            _lookAction = _input.actions["Look"];
         }
-
+        
         void Update()
         {
-         
+            Look();
         }
-
+        
         void FixedUpdate()
         {
-            MovePlayer();
+            Move();
         }
+        #endregion
 
-        /// <summary>
-        /// Get current position of the mouse (if using mouse & keyboard) and calculate the angle the player should rotate to. Works well with WASD keys.
-        /// </summary>
-        void GetMousePosition()
-        {
-            /*Ray ray = Camera.main.ScreenPointToRay(_aim);
-            Plane s_plane = new (Vector3.up, Vector3.zero);
-            
-            if (s_plane.Raycast(ray, out float rayDistance))
-            {
-                Vector3 point = ray.GetPoint(rayDistance);
-                LookAt(point);
-            }*/
-        }
-
-        void LookAt(Vector3 lookPoint)
-        {
-            Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
-            transform.LookAt(heightCorrectedPoint);
-        }
-
-        /// <summary>
-        /// Get keyboard from the player from keyboard or controller. Override mouse angle if using keyboard only or controller
-        /// </summary>
-        void GetPlayerInput()
-        {
-            
-        }
-
+        #region Movement
         /// <summary>
         /// Use the calculated _movementVector to move the player accordingly
         /// </summary>
-        void MovePlayer()
+        void Move(bool Fixed=true)
         {
-            _body.velocity = new Vector3(_movementVector.x * _movementSpeed, 0f, _movementVector.z * _movementSpeed);
+            // read input
+            Vector2 move = _moveAction.ReadValue<Vector2>();
+            
+            // apply speed multiplier and normalize time
+            float normFactor = Fixed ? Time.fixedDeltaTime : Time.deltaTime;
+            move *= _movementSpeed * normFactor;
+            
+            // apply
+            _body.velocity = new Vector3(move.x, 0f, move.y);
         }
+        #endregion
+
+        #region Aim
+        /// <summary>
+        /// Rotates the player with input-device specific logic
+        /// </summary>
+        void Look()
+        {
+            // exit if no input
+            if (_lookAction.activeControl == null) return;
+            
+            // check device type
+            InputDevice device = _lookAction.activeControl.device;
+            bool isMouse = device is Pointer;
+            
+            // read input
+            Vector2 lookValue = _lookAction.ReadValue<Vector2>();
+
+            // choose Look method based on device type
+            if (isMouse)
+            {
+                LookMouse(lookValue);
+            }
+            else
+            {
+                LookController(lookValue);
+            }
+        }
+
+        /// <summary>
+        /// Rotates the player to look toward the mouse.
+        /// </summary>
+        /// <param name="position"> Mouse position in screen space. </param>
+        void LookMouse(Vector2 position)
+        {
+            // get mouse position in world
+            Ray ray = _input.camera.ScreenPointToRay(position);
+            s_groundPlane.Raycast(ray, out float distance);
+            Vector3 mouseInWorld = ray.GetPoint(distance);
+
+            // rotate toward mouse
+            mouseInWorld.y = transform.position.y;
+            transform.LookAt(mouseInWorld);
+        }
+
+        /// <summary>
+        /// Rotates the player to look in the given direction.
+        /// </summary>
+        /// <param name="direction"> Gamepad, or Joystick, direction. </param>
+        void LookController(Vector2 direction)
+        {
+            if (direction.sqrMagnitude < _lookDeadZone) return;
+            
+            Vector3 worldDir = new Vector3(direction.x, 0, direction.y);
+            transform.forward = worldDir;
+        }
+        #endregion
     }
 }
