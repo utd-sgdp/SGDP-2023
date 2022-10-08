6d30a126d45b42b958e0794d53461b9bf545dbb8
PlanesWalker02 <Clayton02.Wheeler05@gmail.com>
2022-10-08 17:17:32 -0500

    Add comments to bomb script
    Added comments to the attack function and made the bomb variable private, but accessible to the editor.

--- Assets/_Game/Scripts/Weapons/WeaponBomb.cs
+++ Assets/_Game/Scripts/Weapons/WeaponBomb.cs
@@ -10,19 +10,23 @@ namespace Game.Weapons
     public class WeaponBomb : WeaponBase
     {
         
-        public float explosionRadius = 5f;
-        public float bombDamage = 50f;
+        [SerializeField] private float _explosionRadius = 5f;
+        [SerializeField] private float _bombDamage = 50f;
+
+        /// <summary>
+        /// Attack method for any enemy with the bomb weapon. All game objects with the Damageable component will take the bomb damage
+        /// </summary>
         public override void Attack()
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRadius);
 
             //Deals damage by calling function from Damageable on everything in radius
             foreach (Collider hit in colliders)
             {                
                 Damageable damaged = hit.gameObject.GetComponent<Damageable>();
-                if (damaged != null)
+                if (damaged != null) // make sure that the collided object has the damageable component before calling a method from that component.
                 {
-                    damaged.Hurt(bombDamage);
+                    damaged.Hurt(_bombDamage);
                 }                
             }
         }
