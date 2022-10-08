147d4772667a889274b896c7af78e0cde649aa9c
Xernacus <a10rec@myfastmail.com>
2022-10-08 14:13:53 -0500

    Comments
    Added comments

--- Assets/_Game/Scripts/Weapons/WeaponBomb.cs
+++ Assets/_Game/Scripts/Weapons/WeaponBomb.cs
@@ -11,14 +11,14 @@ namespace Game.Weapons
     {
         
         public float explosionRadius = 5f;
-        public float bombDamage = 25f;
+        public float bombDamage = 50f;
         public override void Attack()
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-            
-           
+
+            //Deals damage by calling function from Damageable on everything in radius
             foreach (Collider hit in colliders)
-            {
+            {                
                 Damageable damaged = hit.gameObject.GetComponent<Damageable>();
                 if (damaged != null)
                 {
