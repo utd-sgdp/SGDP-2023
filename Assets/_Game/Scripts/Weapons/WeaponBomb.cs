e7fff431ddf64869ead205c9103b036d512be421
Xernacus <a10rec@myfastmail.com>
2022-10-08 00:53:31 -0500

    Damages correctly
    Doesn't break if object doesn't have component

--- Assets/_Game/Scripts/Weapons/WeaponBomb.cs
+++ Assets/_Game/Scripts/Weapons/WeaponBomb.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,21 @@ namespace Game.Weapons
     
     public class WeaponBomb : WeaponBase
     {
+        
         public float explosionRadius = 5f;
         public float bombDamage = 25f;
         public override void Attack()
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-
+            
+           
             foreach (Collider hit in colliders)
             {
-
-                hit.gameObject.GetComponent<Damageable>().Hurt(bombDamage);
-
+                Damageable damaged = hit.gameObject.GetComponent<Damageable>();
+                if (damaged != null)
+                {
+                    damaged.Hurt(bombDamage);
+                }                
             }
         }
     }
