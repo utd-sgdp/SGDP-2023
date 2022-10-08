7dd4d417a3d207fbb691c7789de1ae3022b14269
aregaIado <78439483+aregaIado@users.noreply.github.com>
2022-10-08 13:00:36 -0500

    added UnityEvents
    - added MaxHeal()
    - made Kill() destroy gameObject

--- Assets/_Game/Scripts/player/Damageable.cs
+++ Assets/_Game/Scripts/player/Damageable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game
 {
@@ -10,25 +11,40 @@ namespace Game
         [SerializeField] float _health = 100f;
         [SerializeField] float _maxHealth = 100f;
 
+        public UnityEvent<float> onHeal;
+        public UnityEvent<float> onHurt;
+        public UnityEvent onMaxHeal;
+        public UnityEvent onKill;
+        public UnityEvent onChange;
+
+
         public void Hurt(float amount)
         {
+            onHurt?.Invoke(amount);
+            onChange?.Invoke();
+
             _health -= amount;
-            if(_health <= 0)
-            {
-                Kill();
-            }
+            if(_health <= 0) Kill();
         }
         public void Heal(float amount)
         {
-            _health += amount;
-            if(_health > _maxHealth)
-            {
-                _health = _maxHealth;
-            }
+            onHeal?.Invoke(amount);
+            onChange?.Invoke();
+
+            _health = Mathf.Min(_health + amount, _maxHealth);
+        }
+        public void MaxHeal()
+        {
+            onMaxHeal?.Invoke();
+            onChange?.Invoke();
+
+            _health = _maxHealth;
         }
         public void Kill()
         {
+            onKill?.Invoke();
 
+            Destroy(gameObject);
         }
     }
 }
