428d92a479477f3b9e1467ec179673974b558d64
Jarod Shelton <jarodshelton01@gmail.com>
2022-10-09 04:55:17 -0500

    Refactored Player Movement
    Made two movement styles to test: Acceleration With Drag and Constant Velocity.
    
    Acceleration With Drag is a new equation. Constant Velocity is the old equation with some bug fixes.

--- Assets/_Game/Scripts/player/PlayerMovement.cs
+++ Assets/_Game/Scripts/player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,8 +12,15 @@ namespace Game.Player
     {
         [Header("Movement")]
         [SerializeField]
+        MovementType _movementType = MovementType.AccelerationWithDrag;
+
+        [SerializeField]
         [Min(0)]
-        float _movementSpeed = 500f;
+        float _maxVelocity = 500f;
+
+        [SerializeField]
+        [Min(0)]
+        float _timeToMaxVelocity = 0.25f;
         
         [Header("Look")]
         [SerializeField]
@@ -23,8 +31,21 @@ namespace Game.Player
         PlayerInput _input;
         InputAction _moveAction;
         InputAction _lookAction;
-        
+
+        private float _acceleration = 0;
+        private float _dragCoefficient = 0;
+        private float _velocityMargin = 0.5f;
+
+        // Movement type describes how the player's movement is calculated
+        private enum MovementType
+        {
+            AccelerationWithDrag = 0,
+            ConstantVelocity = 1
+        }
+
+
         static Plane s_groundPlane = new (Vector3.up, Vector3.zero);
+
 
         #region MonoBehaviour
         void Awake()
@@ -34,6 +55,10 @@ namespace Game.Player
             _input = GetComponent<PlayerInput>();
             _moveAction = _input.actions["Move"];
             _lookAction = _input.actions["Look"];
+
+            // Initiallize movement physics values
+            _acceleration = _maxVelocity / _timeToMaxVelocity * lambertW(_maxVelocity / (_velocityMargin));
+            _dragCoefficient = _acceleration / _maxVelocity;
         }
         
         void Update()
@@ -54,14 +79,50 @@ namespace Game.Player
         void Move(bool Fixed=true)
         {
             // read input
-            Vector2 move = _moveAction.ReadValue<Vector2>();
-            
-            // apply speed multiplier and normalize time
+            Vector3 move = _moveAction.ReadValue<Vector2>();
+
+            // convert move input to vector3 and normalize
+            move = new Vector3(move.x, 0, move.y);
+            move = move.normalized;
+
+            // normalize time
             float normFactor = Fixed ? Time.fixedDeltaTime : Time.deltaTime;
-            move *= _movementSpeed * normFactor;
+
+            // The acceleration vector contains how much the velocity will change in a update cycle. This must be declared outside the switch statement
+            Vector3 accelerationVector = Vector3.zero;
+
+            // switch on movement type to determine how to calculate the movement equation
+            switch (_movementType)
+            {
+                // This case simulates the player accelerating with drag. It's meant to be the weightiest movement option. Feels like Dusk.
+                case MovementType.AccelerationWithDrag:
+
+                    // set drag force
+                    accelerationVector = -_body.velocity.normalized * _body.velocity.magnitude * _dragCoefficient * normFactor;
+
+                    // add acceleration if the player is holding a movement input
+                    if (move.magnitude > 0)
+                        accelerationVector += move * _acceleration * normFactor;
+
+                    // set the velocity to zero or max velocity if it is within the velocity margin
+                    if (move.magnitude <= 0 && _body.velocity.magnitude < _velocityMargin)
+                        move = Vector3.zero;
+                    else if (_maxVelocity - _body.velocity.magnitude < _velocityMargin)
+                        move = _body.velocity.normalized * _maxVelocity;
+
+                    // set movement
+                    move = _body.velocity + accelerationVector;
+                    break;
+
+                // This simulates constant velocity. It is the more responsive option. Very snappy, feels like Hollow Knight.
+                case MovementType.ConstantVelocity:
+                    move *= _maxVelocity;
+                    break;
+            }
             
             // apply
-            _body.velocity = new Vector3(move.x, 0f, move.y);
+            _body.velocity = move;
+            
         }
         #endregion
 
@@ -119,6 +180,27 @@ namespace Game.Player
             Vector3 worldDir = new Vector3(direction.x, 0, direction.y);
             transform.forward = worldDir;
         }
+        #endregion
+
+        #region Helper Functions
+
+        /// <summary>
+        /// Calculates the lambertW function for float
+        /// </summary>
+        private float lambertW(float x)
+        {
+            if (x < -Math.Exp(-1))
+                throw new Exception("The LambertW-function is not defined for " + x + ".");
+
+            int amountOfIterations = Mathf.Max(4, (int)Mathf.Ceil(Mathf.Log10(x) / 3));
+            float w = 3 * Mathf.Log(x + 1) / 4;
+
+            for (int i = 0; i < amountOfIterations; i++)
+                w = w - (w * Mathf.Exp(w) - x) / (Mathf.Exp(w) * (w + 1) - (w + 2) * (w * Mathf.Exp(w) - x) / (2 * w + 2));
+
+            return w;
+        }
+
         #endregion
     }
 }
