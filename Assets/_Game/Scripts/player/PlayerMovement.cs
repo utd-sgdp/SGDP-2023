243c0aa2963a4dcd85a9d4b90502868425feb5d0
Jarod Shelton <jarodshelton01@gmail.com>
2022-10-09 06:32:40 -0500

    Implemented player dash

--- Assets/_Game/Scripts/player/PlayerMovement.cs
+++ Assets/_Game/Scripts/player/PlayerMovement.cs
@@ -16,12 +16,25 @@ namespace Game.Player
 
         [SerializeField]
         [Min(0)]
-        float _maxVelocity = 500f;
+        float _maxVelocity = 15f;
 
         [SerializeField]
         [Min(0)]
         float _timeToMaxVelocity = 0.25f;
-        
+
+        [Header("Dash")]
+        [SerializeField]
+        [Min(0)]
+        float _dashLength = 15f;
+
+        [SerializeField]
+        [Min(0)]
+        float _dashDuration = 0.5f;
+
+        [SerializeField]
+        [Min(0)]
+        float _dashCoolDown = 0.5f;
+
         [Header("Look")]
         [SerializeField]
         [Range(0, 1)]
@@ -32,9 +45,19 @@ namespace Game.Player
         InputAction _moveAction;
         InputAction _lookAction;
 
+        public PlayerState _state = PlayerState.FreeRoam;
+        private bool _canDash = true;
+        private float _dashVelocity = 0f;
         private float _acceleration = 0;
         private float _dragCoefficient = 0;
         private float _velocityMargin = 0.5f;
+
+        // Player State describes what state the player is in and may be used to determine the behaviours of the player in a given state
+        public enum PlayerState
+        {
+            FreeRoam = 0,
+            Dash = 1
+        }
 
         // Movement type describes how the player's movement is calculated
         private enum MovementType
@@ -43,9 +66,7 @@ namespace Game.Player
             ConstantVelocity = 1
         }
 
-
         static Plane s_groundPlane = new (Vector3.up, Vector3.zero);
-
 
         #region MonoBehaviour
         void Awake()
@@ -59,6 +80,7 @@ namespace Game.Player
             // Initiallize movement physics values
             _acceleration = _maxVelocity / _timeToMaxVelocity * lambertW(_maxVelocity / (_velocityMargin));
             _dragCoefficient = _acceleration / _maxVelocity;
+            _dashVelocity = _dashLength / _dashDuration;
         }
         
         void Update()
@@ -76,54 +98,91 @@ namespace Game.Player
         /// <summary>
         /// Use the calculated _movementVector to move the player accordingly
         /// </summary>
-        void Move(bool Fixed=true)
+        void Move(bool Fixed = true)
         {
             // read input
             Vector3 move = _moveAction.ReadValue<Vector2>();
+            InputAction dashInput = _input.actions["Dash"];
 
             // convert move input to vector3 and normalize
             move = new Vector3(move.x, 0, move.y);
             move = move.normalized;
-
-            // normalize time
-            float normFactor = Fixed ? Time.fixedDeltaTime : Time.deltaTime;
-
-            // The acceleration vector contains how much the velocity will change in a update cycle. This must be declared outside the switch statement
-            Vector3 accelerationVector = Vector3.zero;
-
-            // switch on movement type to determine how to calculate the movement equation
-            switch (_movementType)
+            
+            // Start the dash coroutine if player is not dashing and pressed the dash button
+            if (_canDash && dashInput.IsPressed())
             {
-                // This case simulates the player accelerating with drag. It's meant to be the weightiest movement option. Feels like Dusk.
-                case MovementType.AccelerationWithDrag:
-
-                    // set drag force
-                    accelerationVector = -_body.velocity.normalized * _body.velocity.magnitude * _dragCoefficient * normFactor;
-
-                    // add acceleration if the player is holding a movement input
-                    if (move.magnitude > 0)
-                        accelerationVector += move * _acceleration * normFactor;
-
-                    // set the velocity to zero or max velocity if it is within the velocity margin
-                    if (move.magnitude <= 0 && _body.velocity.magnitude < _velocityMargin)
-                        move = Vector3.zero;
-                    else if (_maxVelocity - _body.velocity.magnitude < _velocityMargin)
-                        move = _body.velocity.normalized * _maxVelocity;
-
-                    // set movement
-                    move = _body.velocity + accelerationVector;
-                    break;
-
-                // This simulates constant velocity. It is the more responsive option. Very snappy, feels like Hollow Knight.
-                case MovementType.ConstantVelocity:
-                    move *= _maxVelocity;
-                    break;
+                StartCoroutine(Dash(move));
             }
-            
-            // apply
-            _body.velocity = move;
-            
+
+            // Only move if the player state is free roam and allows movement.
+            if(_state == PlayerState.FreeRoam){
+                // normalize time
+                float normFactor = Fixed ? Time.fixedDeltaTime : Time.deltaTime;
+
+                // switch on movement type to determine how to calculate the movement equation
+                switch (_movementType)
+                {
+                    // This case simulates the player accelerating with drag. It's meant to be the weightiest movement option. Feels like Dusk.
+                    case MovementType.AccelerationWithDrag:
+                        Vector3 accelerationVector = Vector3.zero;
+
+                        // set drag force
+                        accelerationVector = -_body.velocity.normalized * _body.velocity.magnitude * _dragCoefficient * normFactor;
+
+                        // add acceleration if the player is holding a movement input
+                        if (move.magnitude > 0)
+                            accelerationVector += move * _acceleration * normFactor;
+
+                        // set the velocity to zero or max velocity if it is within the velocity margin
+                        if (move.magnitude <= 0 && _body.velocity.magnitude < _velocityMargin)
+                            move = Vector3.zero;
+                        else if (_maxVelocity - _body.velocity.magnitude < _velocityMargin)
+                            move = _body.velocity.normalized * _maxVelocity;
+
+                        // set movement
+                        move = _body.velocity + accelerationVector;
+                        break;
+
+                    // This simulates constant velocity. It is the more responsive option. Very snappy, feels like Hollow Knight.
+                    case MovementType.ConstantVelocity:
+                        // Sets the velocity of the player
+                        move *= _maxVelocity;
+                        break;
+                }
+
+                // apply
+                _body.velocity = move;
+            }
         }
+
+        /// <summary>
+        ///  Dash is a coroutine that moves the player in the direction of a dash and the handles the cooldown of the dash
+        /// </summary>
+        IEnumerator Dash(Vector3 direction)
+        {
+            // Set canDash to false so a dash can't interupt the current dash
+            _canDash = false;
+
+            // Set the velocity of the player to the velocity of the dash
+            _body.velocity = direction.normalized * _dashVelocity;
+
+            // Set the players state to dash to prevent movement during the dash
+            _state = PlayerState.Dash;
+
+            // Wait for the span of time the dash will occur
+            yield return new WaitForSeconds(_dashDuration);
+
+            // Set state back to free roam to allow player movement
+            _state = PlayerState.FreeRoam;
+
+            // Zero out the player's velocity
+            _body.velocity = Vector3.zero;
+
+            // Set canDash to true once the cooldown is complete
+            yield return new WaitForSeconds(_dashCoolDown);
+            _canDash = true;
+        }
+
         #endregion
 
         #region Aim
