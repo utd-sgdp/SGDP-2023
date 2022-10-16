41bf387f08cc8b1577f43962a380a13834c3be36
MichaelSpl <splintermic@gmail.com>
2022-10-16 16:12:20 -0500

    Gave Win Menu its own script.

--- Assets/_Game/Scripts/UI/EndMenu.cs
+++ Assets/_Game/Scripts/UI/EndMenu.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-    public class EndMenu : MonoBehaviour
+public class EndMenu : MonoBehaviour
+{
+    // Makes menu dissapear.
+    public void hideMenu()
     {
-        // Makes menu dissapear.
-        public void hideMenu()
-        {
-            gameObject.SetActive(false);
-        }
-
-        // Makes menu appear on screen.
-        public void showMenu()
-        {
-            gameObject.SetActive(true);
-        }
-
-       // Sends player back to starting level.
-        public void restartLevel()
-        {
-            // Will load the next scene when it's ready.
-            // SceneManager.LoadScene("");
-            Debug.Log("Restarting run");
-        }
-
-    // Sends player back to HUB.
-    public void returnToHUB()
-    {
-        // Will load the next scene when it's ready.
-        // SceneManager.LoadScene("");
-        Debug.Log("Returning to HUB");
+        gameObject.SetActive(false);
     }
-}
+
+    // Makes menu appear on screen.
+    public void showMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+   // Sends player back to starting level.
+   public void restartLevel()
+   {
+       // Will load the next scene when it's ready.
+       // SceneManager.LoadScene("");
+       Debug.Log("Restarting run");
+   }
+
+        // Sends player back to HUB.
+   public void returnToHUB()
+   {
+       // Will load the next scene when it's ready.
+       // SceneManager.LoadScene("");
+       Debug.Log("Returning to HUB");
+   }
+}  
 
