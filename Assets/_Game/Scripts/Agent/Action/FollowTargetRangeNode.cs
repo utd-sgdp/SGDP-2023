f82727e6d6b4306471b8c6efdf4df8f521ae9a6b
Xernacus <a10rec@myfastmail.com>
2022-11-11 21:26:35 -0600

    Turret + Summoner
    Added Turret and Summoner enemies. FollowTargetRangeNode now doesn't recalculate destination unless it's destination is not far enough away from target or it reaches it's current destination or it is far enough away.

--- Assets/_Game/Scripts/Agent/Action/FollowTargetRangeNode.cs
+++ Assets/_Game/Scripts/Agent/Action/FollowTargetRangeNode.cs
@@ -25,6 +25,7 @@ namespace Game.Enemy.Action
         public float desiredDistanceMax;
         public float desiredDistanceMin;
         public float moveAwayRandomRotation = 0.15f;
+        public bool moveAwayValid = true;
         
         protected override void OnStart()
         {
@@ -37,46 +38,56 @@ namespace Game.Enemy.Action
         protected override State OnUpdate()
         {
             //Debug.Log($"{Vector3.Distance(Blackboard.transform.position, Blackboard.target.position)}");
-
             float distanceFromTargetSQ = (Blackboard.target.position - Blackboard.transform.position).sqrMagnitude;
             float desiredSQ = desiredDistanceMax * desiredDistanceMax; 
             if (distanceFromTargetSQ > desiredSQ)
             {
                 s_agent.destination = Blackboard.target.position;
                 s_agent.isStopped = false;
+                moveAwayValid = true;
                 return State.Running;
             }
             desiredSQ = desiredDistanceMin * desiredDistanceMin;
+            if (s_agent.destination != null && ((Blackboard.transform.position - s_agent.destination).sqrMagnitude < s_agent.stoppingDistance * s_agent.stoppingDistance || (s_agent.destination - Blackboard.target.position).sqrMagnitude < desiredSQ))
+            {
+                moveAwayValid = true;
+            }
             if (Blackboard.movementReference != null)
             {
-                if (distanceFromTargetSQ <= desiredSQ)
+                if (distanceFromTargetSQ <= desiredSQ && moveAwayValid)
                 {
                     var direction = (Blackboard.target.position - Blackboard.movementReference.position).normalized;
 
-                    // TODO: don't recalculate destination if still valid
-                    // calculate movement away from target                
+                    // calculate movement away from target             
                     float angle = Random.Range(-moveAwayRandomRotation, moveAwayRandomRotation);
                     Vector3 nDest = Quaternion.Euler(0, angle, 0) * direction * (desiredDistanceMin + s_agent.stoppingDistance);
                     s_agent.destination = nDest;
                     s_agent.isStopped = false;
+                    moveAwayValid = false;
                     return State.Running;
                 }
             }
-            if (distanceFromTargetSQ <= desiredSQ)
+            else if (distanceFromTargetSQ <= desiredSQ && moveAwayValid)
             {
                 var direction = (Blackboard.transform.position - Blackboard.target.position).normalized;
-
-                // TODO: don't recalculate destination if still valid
+                
                 // calculate movement away from target                
                 float angle = Random.Range(-moveAwayRandomRotation, moveAwayRandomRotation);
                 Vector3 nDest = Quaternion.Euler(0, angle, 0) * direction * (desiredDistanceMin + s_agent.stoppingDistance);
                 s_agent.destination = nDest;
                 s_agent.isStopped = false;
+                moveAwayValid = false;
                 return State.Running;
             }
 
-            s_agent.isStopped = true;
-            return State.Success;
+            if (distanceFromTargetSQ > desiredDistanceMin * desiredDistanceMin)
+            {
+                s_agent.isStopped = true;
+                moveAwayValid = true;
+                return State.Success;
+            }
+            
+            return State.Running;
         }
     }
 }
