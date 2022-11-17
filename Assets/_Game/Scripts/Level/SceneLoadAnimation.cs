d6daa386562c68865b345522aac41247a80794ca
JoshAltshuler <Josh.Altshuler1@gmail.com>
2022-11-16 21:07:27 -0600

    added comments and fixed the namespace

--- Assets/_Game/Scripts/Level/SceneLoadAnimation.cs
+++ Assets/_Game/Scripts/Level/SceneLoadAnimation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Game
+namespace Game.Level
 {
     public class SceneLoadAnimation : MonoBehaviour
     {
@@ -15,15 +15,24 @@ namespace Game
         private void Start()
         {
             LoadBar.fillAmount = 0;
+
+            //tell the GameScene script to load the scene
             GameScene.Load(SceneIndex.Game);
+
+            //prevent the GameScene from switching to the new scene once it's loaded
             GameScene.LoadOperation.allowSceneActivation = false;
         }
         void Update() => UpdateLoadBar();
         void UpdateLoadBar()
         {
+            //get the loading progress
             currentValue = GameScene.LoadOperation.progress / .9f;
             print(currentValue);
+
+            //set the bar's fill amount to the loading progress
             LoadBar.fillAmount = currentValue;
+
+            //if it has loaded, allow the GameScene to switch to the scene
             if (Mathf.Approximately(currentValue, 1))
             {
                 GameScene.LoadOperation.allowSceneActivation = true;
