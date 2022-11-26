1f3081469bb6b317e4108d950e532b34cd521bc1
Brandon Coffey <bmc939@gmail.com>
2022-11-26 16:47:32 -0600

    Settings Menu Setup

--- Assets/_Game/Scripts/Sound/Managers/AudioMixerController.cs
+++ Assets/_Game/Scripts/Sound/Managers/AudioMixerController.cs
@@ -1,31 +1,57 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace Game.SoundSystem
 {
     public class AudioMixerController : MonoBehaviour
     {
-        [SerializeField] private AudioMixer _mixer = null;
-        [SerializeField] private float _startValue = 0.75f;
+	    [SerializeField] private AudioMixer _mixer = null;
+	    [SerializeField] private Slider _masterSlider;
+	    [SerializeField] private Slider _musicSlider;
+	    [SerializeField] private Slider _ambientSlider;
+	    [SerializeField] private Slider _sfxSlider;
+	    [SerializeField] private string _masterVolume = "MasterVolume";
         [SerializeField] private string _musicVolume = "MusicVolume";
-        [SerializeField] private string _sfxVolume = "SfxVolume";
+	    [SerializeField] private string _ambientVolume = "AmbientVolume";
+	    [SerializeField] private string _sfxVolume = "SfxVolume";
+        
+	    private void Awake()
+	    {
+	    	if (PlayerPrefs.HasKey(_masterVolume)) _masterSlider.value = PlayerPrefs.GetFloat(_masterVolume);
+	    	if (PlayerPrefs.HasKey(_musicVolume)) _musicSlider.value = PlayerPrefs.GetFloat(_musicVolume);
+	    	if (PlayerPrefs.HasKey(_ambientVolume)) _ambientSlider.value = PlayerPrefs.GetFloat(_ambientVolume);
+	    	if (PlayerPrefs.HasKey(_sfxVolume)) _sfxSlider.value = PlayerPrefs.GetFloat(_sfxVolume);
+	    	SetMasterVolume(_masterSlider.value);
+	    	SetMusicVolume(_musicSlider.value);
+	    	SetAmbientVolume(_ambientSlider.value);
+	    	SetSfxVolume(_sfxSlider.value);
+	    }
 
-        private void Start()
-        {
-            SetMusicVolume(_startValue);
-            SetSfxVolume(_startValue);
-        }
+	    public void SetMasterVolume(float volume)
+	    {
+		    _mixer.SetFloat(_musicVolume, ConvertVolume(volume));
+		    PlayerPrefs.SetFloat(_masterVolume, volume);
+	    }
 
         public void SetMusicVolume(float volume)
         {
-            if (_mixer == null) return;
-            _mixer.SetFloat(_musicVolume, Mathf.Log10(volume) * 20);
+	        _mixer.SetFloat(_musicVolume, ConvertVolume(volume));
+	        PlayerPrefs.SetFloat(_musicVolume, volume);
         }
+        
+	    public void SetAmbientVolume(float volume)
+	    {
+		    _mixer.SetFloat(_ambientVolume, ConvertVolume(volume));
+		    PlayerPrefs.SetFloat(_ambientVolume, volume);
+	    }
 
         public void SetSfxVolume(float volume)
         {
-            if (_mixer == null) return;
-            _mixer.SetFloat(_sfxVolume, Mathf.Log10(volume) * 20);
+	        _mixer.SetFloat(_sfxVolume, ConvertVolume(volume));
+	        PlayerPrefs.SetFloat(_sfxVolume, volume);
         }
+        
+	    private static float ConvertVolume(float volume) => volume > 0 ? Mathf.Log10(volume) * 20 : -80;
     }
 }
